b890e482e3ad7cc8d90fcf04ed8d24f35a64c9fb
Daniel Lima <danielslz@gmail.com>
2025-01-20 08:49:57 -0300

    Commit inicial

---
+++ fish/config.fish
@@ -0,0 +1,42 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Starship
+starship init fish | source
+
+# Fastfetch
+# function fish_greeting
+#    fastfetch
+# end
+
+# Pyenv
+pyenv init - fish | source
+status --is-interactive; and pyenv virtualenv-init - | source
+
+# File system
+alias ls='eza -lh --group-directories-first --icons'
+alias lsa='ls -a'
+alias lt='eza --tree --level=2 --long --icons --git'
+alias lta='lt -a'
+alias ff="fzf --preview 'batcat --style=numbers --color=always {}'"
+alias fd='fdfind'
+
+# Directories
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+
+# Tools
+alias n='nvim'
+alias g='git'
+alias d='docker'
+alias r='rails'
+alias bat='batcat'
+alias lzg='lazygit'
+alias lzd='lazydocker'
+
+# Git
+alias gcm='git commit -m'
+alias gcam='git commit -a -m'
+alias gcad='git commit -a --amend'
