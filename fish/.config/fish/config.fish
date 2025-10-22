d9215836590b07af28fb3c7aefa3fe680375abc3
Daniel Lima <danielslz@users.noreply.github.com>
2025-10-22 07:34:13 -0300

    Update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,10 +13,6 @@ starship init fish | source
 #    fastfetch
 # end
 
-# Pyenv
-# pyenv init - fish | source
-# status --is-interactive; and pyenv virtualenv-init - | source
-
 # File system
 alias ls='eza -lh --group-directories-first --icons'
 alias lsa='ls -a'
@@ -25,21 +21,3 @@ alias lta='lt -a'
 alias ff="fzf --preview 'batcat --style=numbers --color=always {}'"
 alias fd='fdfind'
 
-# Directories
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-
-# Tools
-alias n='nvim'
-alias g='git'
-alias d='docker'
-alias r='rails'
-alias bat='batcat'
-alias lzg='lazygit'
-alias lzd='lazydocker'
-
-# Git
-alias gcm='git commit -m'
-alias gcam='git commit -a -m'
-alias gcad='git commit -a --amend'
